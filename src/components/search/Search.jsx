fde320a81081ff09245b99754c0ca96aa8c5f443
mkr123 <mankairui@yahoo.com>
2021-12-09 12:13:57 -0800

    create search component root

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Fragment } from "react"
+export default Search = (props) => {
+  return (
+    <React.Fragment>
+      {/* <SearchSection ></SearchSection>
+      <ResultSection ></ResultSection> */}
+    </React.Fragment>
+  )
+
+}
