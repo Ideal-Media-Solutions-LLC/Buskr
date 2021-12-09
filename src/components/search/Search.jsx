2ae22322ec17a78b1486ca74b8c3ebc343aefe9c
mkr123 <mankairui@yahoo.com>
2021-12-09 14:01:26 -0800

    fix bugs on search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import SearchSection from "./SearchSection.jsx";
+import React from 'react';
+import SearchSection from './SearchSection.jsx';
 const Search = (props) => {
   return (
-    <div id="searchContainer">
+    <div id='searchContainer'>
       <SearchSection ></SearchSection>
       {/*
       <ResultSection ></ResultSection> */}
