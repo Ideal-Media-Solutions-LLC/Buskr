700dce67bb5eead2f898bfeef6ff9eedcc663289
mkr123 <mankairui@yahoo.com>
2021-12-09 14:15:02 -0800

    merge from main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import Search from '../components/search/Search.jsx';
-=======
->>>>>>> 9c709afaf00a64eb894db38b84e1bfbbe14904ce
 
 export default function Home() {
   return (
     <div>
       Buskr App
+      <Search />
     </div>
   );
 }
