9087cfd9a55df4f012d9d33ea32da22f55db30f4
Eun Ji Roh <eunjiroh@gmail.com>
2021-12-09 21:59:57 -0800

    created a resultSection component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SearchSection from './SearchSection.jsx';
+import ResultSection from './test/ResultSection.jsx';
+
 const Search = (props) => {
   return (
     <div id='searchContainer'>
-      <SearchSection ></SearchSection>
-      {/*
-      <ResultSection ></ResultSection> */}
+      <SearchSection></SearchSection>
+      <ResultSection />
     </div>
   );
 
