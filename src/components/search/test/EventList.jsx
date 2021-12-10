021e60e25764d2401a8873c3ea30eabe5762c0bb
Eun Ji Roh <eunjiroh@gmail.com>
2021-12-09 22:01:18 -0800

    created eventList and eventItem components

---
+++ src/components/search/test/EventList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import EventItem from './EventItem.jsx';
+
+const EventList = () => {
+  return (
+    <div className="event-list-Container">
+      {/* {[].map(
+        (event) => { */}
+      <EventItem />;
+      {/* }
+      )} */}
+    </div>
+  );
+};
+
+export default EventList;
