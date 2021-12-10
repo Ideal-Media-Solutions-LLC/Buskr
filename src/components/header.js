6cc1657ec54b002115f873a106cec00c4c70a64f
Michael Fornwalt <mdfornwalt@gmail.com>
2021-12-09 17:59:09 -0700

    header implemented

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,12 @@ import styles from '../styles/header.module.css';
 const Header = () => (
   <div className={styles.headerContainer}>
     <div className={styles.hamburgerIconLogoContainer}>
-      <img className={styles.hamburger} src='src/imgs/buskr-hamburger.gif'/>
-      <img className={styles.icon} src='src/imgs/buskr-icon.gif'/>
-
-      <img className={styles.logo} src='src/imgs/buskr-logo.png'/>
+      <img className={styles.hamburger} src='imgs/buskr-hamburger.gif'/>
+      <img className={styles.icon}src='imgs/buskr-icon.gif'/>
+      <img className={styles.logo}src='imgs/buskr-logo.png'/>
     </div>
     <div className={styles.loginButton}>
-      login
+      LOGIN / REGISTER
     </div>
   </div>
 );
