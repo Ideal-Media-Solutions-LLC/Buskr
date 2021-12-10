53224cb0b9df13c1ff29d279fe927b8f37ff6d75
Michael Fornwalt <mdfornwalt@gmail.com>
2021-12-09 19:15:31 -0700

    added / to img src dirs

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ import styles from '../styles/header.module.css';
 const Header = () => (
   <div className={styles.headerContainer}>
     <div className={styles.hamburgerIconLogoContainer}>
-      <img className={styles.hamburger} src='imgs/buskr-hamburger.gif'/>
-      <img className={styles.icon}src='imgs/buskr-icon.gif'/>
-      <img className={styles.logo}src='imgs/buskr-logo.png'/>
+      <img className={styles.hamburger} src='/imgs/buskr-hamburger.gif'/>
+      <img className={styles.icon}src='/imgs/buskr-icon.gif'/>
+      <img className={styles.logo}src='/imgs/buskr-logo.png'/>
     </div>
     <div className={styles.loginButton}>
       LOGIN / REGISTER
