f9287cdf313f4a02c65f6ec8773e3443ef04169d
Michael Fornwalt <mdfornwalt@gmail.com>
2021-12-10 15:34:45 -0700

    Profile page implemented.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Search from '../components/search/Search.jsx';
+import Profile from '../components/profile/Profile.jsx';
+//This is dummy data for Profile team - will be replaced with API
+import profileData from '../components/profile/profileData.js';
 
 export default function Home() {
   return (
     <div>
-      Buskr App
-      <Search />
+      {/* <Search /> */}
+      {/* <Profile profileData={profileData}/> */}
     </div>
   );
 }
