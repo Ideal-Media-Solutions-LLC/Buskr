66f02842e5e83ba15cf70091128eff90d461eb56
jonathanjewett <jonathanjewett0@gmail.com>
2021-12-11 11:51:48 -0600

    commit before pulling calendar-css

--- src/components/calendar/CalendarView.jsx
+++ src/components/calendar/CalendarView.jsx
@@ -2,17 +2,21 @@ import Calendar from 'react-calendar';
 import React, { useEffect, useState } from 'react';
 
 const CalendarView = () => {
+  // updates when a date is clicked, will tie this with search function
   const [date, setDate] = useState(new Date());
+  // fake event data, will replace with API data later
   const eventDates = [1, 2, 4, 10, 20, 25, 31];
   return (
     <div>
       <h1>Calendar</h1>
       <Calendar
-        calendarType={"US"}
-        onChange={setDate}
+        onClickDay={setDate}
         value={date}
         tileContent={({ date, view }) => view === 'month' && eventDates.includes(date.getDate()) ? <div className="notification">*</div> : null}
       />
+      <div>
+        <h3>{date.toDateString()}</h3>
+      </div>
     </div>
   );
 };
