23380fdef2c79016db8dd61223bd17ef8df7d570
Michael Fornwalt <mdfornwalt@gmail.com>
2021-12-11 15:27:41 -0700

    Profile renders Events using <EventItem /> dynamically

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import EventItem from '../search/results/EventItem';
 import styles from '../../styles/Profile.module.css';
 import profileData from './profileData';
 
 const Profile = ({ profileId }) => {
   const [loggedIn, setLoggedIn] = useState();
-  const [performer, setPerformer] = useState(profileData);
+  const [performer, setPerformer] = useState();
 
   useEffect(() => {
     axios.get(`http://www.buskr.life/api/profile/${profileId}`).then(result => {
       console.log('RESULT: ', result.data);
-      // setPerformer(/*result*/);
+      setPerformer(result.data);
     }).catch(err => {
-      console.log(err);
+      console.log('Error attempting GET profile by id: ', err);
     });
   }, [profileId]);
 
   return (
     <div className={styles.profileContainer}>
-      <div className='master-title'>{performer.name}</div>
-      <img className={styles.profileImage} src={performer.photo} alt='profile-image'/>
-      <div className={styles.profileBio}>{performer.bio}</div>
+      <div className='master-title'>{performer?.name}</div>
+      <img className={styles.profileImage} src={performer?.photo} alt='profile-image'/>
+      <div className={styles.profileBio}>{performer?.bio}</div>
       <div className={styles.tipsContainer}>
         {/* change these to links that lead to tips URLs */}
         <img className ={styles.tipIcon} src='/imgs/tip-paypal-40px.png' alt='paypal'/>
@@ -32,7 +33,7 @@ const Profile = ({ profileId }) => {
       <div className='master-title'>Upcoming Events:</div>
       <div className={styles.eventCardsContainer}>
         {/* Map through performer's events, and render an Event Card for each one */}
-        Event cards go here...
+        {performer?.events.map((event, i) => <EventItem key={i} event={event}/>)}
       </div>
     </div>
   );
