b6ceb4a71e358ae2be1d38b3e64f4fa207a79fb2
Michael Fornwalt <mdfornwalt@gmail.com>
2021-12-11 13:14:39 -0700

    GET request and data flow begun for Profile component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Search from '../components/search/Search';
 import Profile from '../components/profile/Profile';
 // This is dummy data for Profile team - will be replaced with API
-import profileData from '../components/profile/profileData';
+// import profileData from '../components/profile/profileData';
 import CreateEvent from '../components/create-event/CreateEvent';
 
 export default function Home() {
   return (
     <div>
-      <Search />
-      {/* <Profile profileData={profileData}/> */}
+      {/* <Search /> */}
+      <Profile profileId={1}/>
       {/* <CreateEvent/> */}
     </div>
   );
