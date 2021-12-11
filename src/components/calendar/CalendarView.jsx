fb4e28c3ef77ff1c30d8f1db1c352e328b5a4a62
jonathanjewett <jonathanjewett0@gmail.com>
2021-12-10 23:13:26 -0600

    commit before pulling from main after CL CSS PR

--- src/components/calendar/CalendarView.jsx
+++ src/components/calendar/CalendarView.jsx
@@ -1,28 +1,18 @@
-import { useRouter } from 'next/router';
 import Calendar from 'react-calendar';
 import React, { useEffect, useState } from 'react';
 
 const CalendarView = () => {
-  // const router = useRouter();
-  // const { id } = router.query;
   const [date, setDate] = useState(new Date());
-
-  // useEffect(async () => {
-  //   const result = await axios.get(`/api/events/${id}`);
-  //   setEventData(result.body);
-  // }, [id]);
-
+  const eventDates = [1, 2, 4, 10, 20, 25, 31];
   return (
     <div>
       <h1>Calendar</h1>
       <Calendar
+        calendarType={"US"}
         onChange={setDate}
         value={date}
+        tileContent={({ date, view }) => view === 'month' && eventDates.includes(date.getDate()) ? <div className="notification">*</div> : null}
       />
-      <p>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Search from '../components/search/Search';
+import CalendarView from '../components/calendar/CalendarView';
 
 export default function Home() {
   return (
     <div>
       Buskr App
       <Search />
+      <CalendarView /> 
     </div>
   );
 }
