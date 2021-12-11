c4e360c10add19183ef6d494725f13e9d6001256
jonathanjewett <jonathanjewett0@gmail.com>
2021-12-11 13:13:01 -0600

    commit before pull

--- src/components/calendar/CalendarView.jsx
+++ src/components/calendar/CalendarView.jsx
@@ -6,22 +6,31 @@ const CalendarView = () => {
   // updates when a date is clicked, will tie this with search function
   const [date, setDate] = useState(new Date());
   // fake event data, will replace with API data later
+  
+  // Rendering the event dots:
+  // "2021-12-10T15:26:40.016685-08:00"
+  // 1. joined table -> like "2021-12*" - all the events on that month
+    // select * 
+    // from   foo 
+    // where  timestamp_field::date = '2015-04-15';
+    // will grab current month and year from activeStartDate
+      // use .getMonth() and .getFullYear() from what is returned, then make request using that information
+  // 2. makes all the "days" into an array
+  // 3. setEventDates to the result array
+  
   const eventDates = [1, 2, 4, 10, 20, 25, 31];
   return (
     <div>
-      <h1>Calendar</h1>
       <Calendar
         onClickDay={setDate}
         value={date}
-        tileContent={({ date, view }) => view === 'month' && eventDates.includes(date.getDate()) ? <div className="notification">*</div> : null}
+        tileContent={({ date, view }) => view === 'month' && eventDates.includes(date.getDate()) ? <div className="notification"></div> : null}
         prevLabel={<BsChevronLeft />}
         nextLabel={<BsChevronRight />}
         next2Label={null}
         prev2Label={null}
+        showNeighboringMonth={false}
       />
-      <div>
-        <h3>{date.toDateString()}</h3>
-      </div>
     </div>
   );
 };
--- src/components/calendar/FakeSearchComponent.jsx
+++ src/components/calendar/FakeSearchComponent.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import SearchSection from '../search/SearchSection';
 import FakeResultSection from './results/FakeResultSection';
 
+// Re-fetch data on dateChange:
+// 1. onClickDay -> trigger onDateChange()
+// 2. onDateChange -> setDate & re-fetch the shared props (props for re-rendering the whole Search component)
+// 3. turn off calendar and show List View (props for view change control)
+
+// fetch(currentDate)
+
 const FakeSearchComponent = (props) => {
   return (
     <div id='searchContainer'>
