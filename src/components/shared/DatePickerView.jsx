6d24837c4ea35c9278a36dc7693f4a3ac12941eb
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-12-15 00:42:00 -0800

    Fixed linter errors.

--- src/components/shared/DatePickerView.jsx
+++ src/components/shared/DatePickerView.jsx
@@ -1,18 +1,17 @@
+import React from 'react';
 import DatePicker from 'react-datepicker';
 
 const DatePickerView = (props) => {
   return (
-    <div className='datepicker-container'>
-      <DatePicker 
-        className={props.className}
-        selected={props.selected}
-        onChange={props.onChange}
-        placeholderText={props.placeholderText}
-        showTimeSelect={props.showTimeSelect}
-        dateFormat={props.dateFormat}
-        calendarIcon={null}
-      />
-    </div>
+    <DatePicker
+      className={props.className}
+      selected={props.selected}
+      onChange={props.onChange}
+      placeholderText={props.placeholderText}
+      showTimeSelect={props.showTimeSelect}
+      dateFormat={props.dateFormat}
+      calendarIcon={null}
+    />
   );
 };
 
