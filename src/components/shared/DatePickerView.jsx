cb443bc146916a4f4275924dc76fc3d44a622c3a
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-12-15 00:19:59 -0800

    Updated the sared DatePickerView with className props.

--- src/components/shared/DatePickerView.jsx
+++ src/components/shared/DatePickerView.jsx
@@ -4,11 +4,13 @@ const DatePickerView = (props) => {
   return (
     <div className='datepicker-container'>
       <DatePicker 
+        className={props.className}
         selected={props.selected}
         onChange={props.onChange}
         placeholderText={props.placeholderText}
         showTimeSelect={props.showTimeSelect}
         dateFormat={props.dateFormat}
+        calendarIcon={null}
       />
     </div>
   );
