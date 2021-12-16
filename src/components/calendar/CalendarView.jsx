5e2110140ed211db29ad51227c428fa591105e89
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-12-15 17:45:59 -0800

    Updated the filtering process so it only renders dots/events that are within 250 distance.

--- src/components/calendar/CalendarView.jsx
+++ src/components/calendar/CalendarView.jsx
@@ -1,13 +1,10 @@
-import Calendar from 'react-calendar';
 import React, { useEffect, useState, useContext } from 'react';
-import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import axios from 'axios';
 import SearchContext from '../search/SearchContext';
+import Calendar from 'react-calendar';
+import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 
 const CalendarView = () => {
-  // grab the current location data through Conext when no location was entered - default location
-  // when the user clicks the Search button, then we would setSearchObj
-  // to the passed down filterObject from Search Team
   const SearchbarContext = useContext(SearchContext);
   const [searchObj, setSearchObj] = useState(SearchbarContext.results.filterWords);
   // updates when a date is clicked, will tie this with search function
@@ -20,7 +17,7 @@ const CalendarView = () => {
   const [currentEndDate, setCurrentEndDate] = useState(
     new Date(currentStartDate.getFullYear(), currentStartDate.getMonth() + 1, 0),
   );
-  const [queryData, setQueryData] = useState(null);
+  // const [queryData, setQueryData] = useState(null);
   const [eventDates, setEventDates] = useState(new Set());
 
   // updates "from" and "to" dates when month is changed in calendar
@@ -32,19 +29,19 @@ const CalendarView = () => {
 
   // performs search with new "from" and "to" dates
   useEffect(() => {
-    axios.get(`https://www.buskr.life/api/events?lng=${searchObj.lng}&lat=${searchObj.lat}&from=${currentStartDate}&to=${currentEndDate}&sort=time`)
+    axios.get(`https://www.buskr.life/api/events?lng=${searchObj.lng}&lat=${searchObj.lat}&from=${currentStartDate}&to=${currentEndDate}&sort=distance`)
       .then(res => {
         // do we need a state for queryData?
-        setQueryData(res.data);
+        // setQueryData(res.data);
         return res.data;
       })
       .then(data => {
         const tempEventDates = new Set();
-        data.features.forEach(event => {
-          const hasKeywords = searchObj.keywords.split(' ').every(keyword => {
-            if (event.properties.name.includes(keyword)
-              || event.properties.buskerName.includes(keyword)
-              || event.properties.description.includes(keyword)) {
+        data.features.filter(event => event.distance <= 250).forEach(event => {
+          const hasKeywords = searchObj.keywords.toLowerCase().split(' ').every(keyword => {
+            if (event.properties.name.toLowerCase().includes(keyword)
+              || event.properties.buskerName.toLowerCase().includes(keyword)
+              || event.properties.description.toLowerCase().includes(keyword)) {
               return true;
             }
             return false;
