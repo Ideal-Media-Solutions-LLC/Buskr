1287dd40697c1dc661ea17b13b917d2b14551d2f
April Granzow <apriljgranzow@gmail.com>
2021-12-16 11:01:35 -0800

    Add link to busker profile from event page

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 import {
   FacebookIcon,
@@ -19,6 +20,7 @@ const Event = function Event({ event }) {
     name,
     tags,
     photos,
+    buskerId,
     buskerName,
     description,
     location: { address, locality, administrative_area_level_1 },
@@ -41,7 +43,11 @@ const Event = function Event({ event }) {
       <img className={styles.eventImage} src={photos[0]} alt={name}/>
       <div className='master-title'>{name}</div>
       <section className={styles.buskerNameContainer}>
-      <div className={styles.buskerName}>By {buskerName}</div>
+      <div className={styles.buskerName}>
+      <Link href={`/profile/${buskerId}`}>
+        { buskerName }
+      </Link>
+      </div>
         <div>
           <FacebookShareButton url={url}>
             <FacebookIcon className={styles.socialIcon}/>
