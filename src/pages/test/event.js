c6187b36b134f8fe4dce43215a658eada834e0f3
April Granzow <apriljgranzow@gmail.com>
2021-12-16 10:26:18 -0800

    Remove accidentally committed test page event

--- src/pages/test/event.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import data from '../../../public/singleSampleEvent.json';
-import Event from '../../components/Event';
-
-export default function eventPage() {
-  return (<Event event={data.features[0]}/>);
-}
