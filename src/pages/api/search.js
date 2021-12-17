8cde1a22bd6b03a8a205c1b3a1f0918ffb70ea0f
Joshua Booth <joshua.n.booth@gmail.com>
2021-12-17 11:27:52 -0800

    Fix geolocation bug

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,25 +1,19 @@
 import axios from 'axios';
 
-// export default async function findCoords(req, res) {
-//   const { address } = req.query;
-//   const { GEOCODE_API_KEY } = process.env;
-//   const { data: [result] } = await axios.get(
 export default async function findCoords(req, res) {
   const { address } = req.query;
-  const { GEOCODE_API_KEY } = process.env;
-  const resultfromApi = await axios.get(
+  const { data: { results: [result] } } = await axios.get(
     'https://maps.googleapis.com/maps/api/geocode/json',
     {
       params: {
         address,
-        key: GEOCODE_API_KEY,
+        key: process.env.GOOGLE_KEY,
       },
     },
   );
-  // if (result === undefined) {
-  //   res.send({});
-  // } else {
-  //   res.send(result.geometry.location);
-  // }
-  res.send(resultfromApi.data.results[0].geometry.location);
+  if (result === undefined) {
+    res.send({});
+  } else {
+    res.send(result.geometry.location);
+  }
 }
