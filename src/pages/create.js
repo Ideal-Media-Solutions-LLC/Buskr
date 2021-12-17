31f7d3888812167bccc30b108a2b3af1b62667ef
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-12-16 17:40:47 -0800

    merged conflict in create.js.

--- src/pages/create.js
+++ src/pages/create.js
@@ -9,16 +9,6 @@ export const getServerSideProps = async function getServerSideProps(context) {
   return { props: { user } };
 };
 
-<<<<<<< HEAD
-const CreateEventRenderer = ({ user }) => (
-  <UserContext.Provider value={user}>
-    <Header />
-    <LocationContext.Subscriber>
-      {center => <CreateEvent center={center}/>}
-    </LocationContext.Subscriber>
-  </UserContext.Provider>
-);
-=======
 const CreateEventRenderer = ({ user }) => {
   const center = useContext(LocationContext);
   return (
@@ -28,6 +18,5 @@ const CreateEventRenderer = ({ user }) => {
     </UserContext.Provider>
   );
 };
->>>>>>> 0a11943ae0972765ce271324209f335930a6618e
 
 export default CreateEventRenderer;
