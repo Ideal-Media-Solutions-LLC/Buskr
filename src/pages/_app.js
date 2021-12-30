f2e4a2a4e01ce9bc03efb1eb86631d5fcf17a611
Joshua Booth <joshua.n.booth@gmail.com>
2021-12-30 12:04:50 -0800

    Use IP to approximate location if geolocation is disabled

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,22 @@ import '../styles/globals.css';
 import '../styles/Calendar.css';
 import '../styles/Datepicker.css';
 import { LocationContext } from '../contexts';
+import { locateIP } from '../interface';
 
 export default function Buskr({ Component, pageProps }) {
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState(null);
+  // It's safe to use asynchronous effects here because the root component is always mounted.
   useEffect(() => {
+    const fallbackToIP = () => locateIP().then(setLocation).catch(console.error);
     if (typeof navigator !== 'undefined' && 'geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-        setLocation({ lat: latitude, lng: longitude });
-      });
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => setLocation({ lat: latitude, lng: longitude }),
+        fallbackToIP,
+      );
+    } else {
+      fallbackToIP();
     }
-  }, []);
+  }, [setLocation]);
   return (
       <LocationContext.Provider value={location}>
         <div className='master-container'>
